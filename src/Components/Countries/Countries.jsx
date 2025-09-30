72ece6d58a528b0b75cf541d5b5b7c9deb8ab612
Nayeem Hasan Shanto <snayeemhasan@gmail.com>
2025-09-30 19:17:13 +0600

    lift up the state practiced

--- src/Components/Countries/Countries.jsx
+++ src/Components/Countries/Countries.jsx
@@ -1,19 +1,54 @@
-import { use } from "react"
+import { use, useState } from "react";
 import Country from "./Country";
 
-export default function Countries({fetchCountries}){
-    const allCountries = use(fetchCountries)
-    const countries = allCountries.countries;
-    console.log(countries)
-    return (
-        <div>
-            <h3 className="text-center text-4xl font-bold mb-5">Total Country: {countries.length}  </h3>
+export default function Countries({ fetchCountries }) {
+  const allCountries = use(fetchCountries);
+  const countries = allCountries.countries;
+  console.log(countries);
 
-            <div className="grid grid-cols-3 gap-4 ">
-                {
-                    countries.map(country => <Country key={country.cca3.cca3} country={country}></Country>)
-                }
-            </div>
-        </div>
-    )
+  /* declaring a state for counting the visited country.... 
+    ........ lift up te state.
+    Lift up the state মানে কী?
+
+React-এ "Lift up the state" বলতে বোঝায় —
+👉 যখন দুই বা তার বেশি কম্পোনেন্টের মধ্যে একই state দরকার হয়, তখন সেই state কে তাদের common parent component এ নিয়ে যাওয়া।
+
+অর্থাৎ, state টা child component-এ না রেখে উপরে (parent এ) তুলে রাখা, যাতে সেটা সবাই শেয়ার করতে পারে।
+
+🔹 কেন দরকার হয়?
+
+ধরো তোমার দুইটা child component আছে, আর দুটোতেই একই data দরকার।
+
+যদি আলাদা আলাদা state রাখো → তাহলে mismatch বা conflict হতে পারে।
+
+তাই state কে parent এ নিয়ে যাওয়া হয় → তারপর props দিয়ে নিচে পাঠানো হয়।
+  */
+ const [visitedCountries, setVisitedCountries]= useState([]);
+ const handleVisitedCountries =(country)=>{
+   const newVisitedCountries = [...visitedCountries, country];
+   setVisitedCountries(newVisitedCountries);
+    /* amra ekhane ekta state declare korsi and and ekta event handel korar jonno function likhsi but ekhane kono button nai handel use korar jonno.. tai amader ei handel function ta ke country component er moddhe props hishebe pathaite hobe..  */
+ }
+
+return (
+    <div>
+      <h3 className="text-center text-4xl font-bold mb-5">
+        Total Country: {countries.length}{" "}
+      </h3>
+      <h3 className="text-center text-xl font-bold mb-5">
+        Total visited Country: {visitedCountries.length}
+      </h3>
+      <ol className=" flex justify-between flex-wrap gap-10 text-center border-2 w-1/2 mx-auto p-4 list-decimal font-bold mb-4 rounded-xl" >
+        {
+            visitedCountries.map(country => <li key={country.cca3.cca3}>{country.name.common}</li>)
+        }
+      </ol>
+
+      <div className="grid grid-cols-1  md:grid-cols-3 gap-4 ">
+        {countries.map((country) => (
+          <Country key={country.cca3.cca3} handleVisitedCountries={handleVisitedCountries} country={country}></Country>
+        ))}
+      </div>
+    </div>
+  );
 }
--- src/Components/Countries/Country.jsx
+++ src/Components/Countries/Country.jsx
@@ -1,17 +1,49 @@
-import React from 'react';
+import React, { useState } from "react";
 
-const Country = ({country}) => {
-    console.log(country)
-    return (
-        <div className='border-2 p-4 rounded-2xl '>
-            <img className='w-full h-[200px] rounded-2xl mb-4' src={country.flags.flags.png} alt={country.flags.flags.alt} />
-            <h3>Country Name: {country.name.common} </h3>
-            <h3>Capital : {country.capital.capital}</h3>
-            <h3>Population: {country.population.population}</h3>
-            <h3>Region: {country.region.region}</h3>
-            <h3>Area: {country.area.area}</h3>
-        </div>
-    );
+const Country = ({ country, handleVisitedCountries }) => {
+    console.log(handleVisitedCountries);
+  const [visited, setvisited] = useState(false);
+
+  const handleVisited = () => {
+    /* conditional rendering use kore toggling */
+    /* toggling */
+    /* if(visited){
+            setvisited(false)
+        }
+        else{
+            setvisited(true);
+        } */
+    /* option 2 for toggling */
+    //    setvisited(visited ? false : true)
+
+    /* option 3 for toggling */
+    setvisited(!visited);
+    handleVisitedCountries(country);
+  };
+  console.log(country);
+  return (
+    /* conditional rendering kore visited country te css add kora hoice . ek sathe normal class and conditional css class use korte hole template string use kore er moddhe korte hobe*/
+    <div className={`border-2 p-4 rounded-2xl ${visited && "bg-amber-100"}`}>
+      <img
+        className="w-full h-[200px] rounded-2xl mb-4"
+        src={country?.flags?.flags?.png}
+        alt={country.flags.flags.alt}
+      />
+      <div className=" text-center font-semibold text-xl">
+        <h3>Country Name: {country.name.common} </h3>
+        <h3>Capital : {country.capital.capital}</h3>
+        <h3>Population: {country.population.population}</h3>
+        <h3>Region: {country.region.region}</h3>
+        <h3>
+          Area: {country.area.area}{" "}
+          {country.area.area > 300000 ? "large Country" : "Small Country"}
+        </h3>
+        <button onClick={handleVisited} className="btn btn-warning">
+          {visited ? "Visited" : "Not-Visited"}
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Country;
